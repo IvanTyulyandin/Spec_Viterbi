d75f6bf4713aa932f7d10e95d78a5c41dddeaa64
IvanTyulyandin <ivan.tyulyandin@gmail.com>
2021-03-22 12:17:36 +0300

    Fix wrong ess test

--- tests/test_ess_reader.cpp
+++ tests/test_ess_reader.cpp
@@ -1,25 +1,10 @@
 #include "../Viterbi_impl/data_reader.h"
 
 int main() {
-    auto hmm = read_HMM("../chmm_files/test_chmm.chmm");
-    auto is_test_passed =
-        (hmm.states_num == 2 &&
-         HMM::almost_equal(hmm.start_probabilities[0], HMM::to_modified_prob(0.5)) &&
-         HMM::almost_equal(hmm.start_probabilities[1], HMM::to_modified_prob(0.5)) &&
-         hmm.emit_num == 4 && HMM::almost_equal(hmm.emissions[0], HMM::to_modified_prob(0.2)) &&
-         HMM::almost_equal(hmm.emissions[1], HMM::to_modified_prob(0.3)) &&
-         HMM::almost_equal(hmm.emissions[2], HMM::to_modified_prob(0.3)) &&
-         HMM::almost_equal(hmm.emissions[3], HMM::to_modified_prob(0.2)) &&
-         HMM::almost_equal(hmm.emissions[4], HMM::to_modified_prob(0.3)) &&
-         HMM::almost_equal(hmm.emissions[5], HMM::to_modified_prob(0.2)) &&
-         HMM::almost_equal(hmm.emissions[6], HMM::to_modified_prob(0.2)) &&
-         HMM::almost_equal(hmm.emissions[7], HMM::to_modified_prob(0.3)) && hmm.trans_num == 4 &&
-         hmm.trans_rows == HMM::Index_vec_t{0, 0, 1, 1} &&
-         hmm.trans_cols == HMM::Index_vec_t{0, 1, 0, 1} &&
-         HMM::almost_equal(hmm.trans_probs[0], HMM::to_modified_prob(0.5)) &&
-         HMM::almost_equal(hmm.trans_probs[1], HMM::to_modified_prob(0.5)) &&
-         HMM::almost_equal(hmm.trans_probs[2], HMM::to_modified_prob(0.4)) &&
-         HMM::almost_equal(hmm.trans_probs[3], HMM::to_modified_prob(0.6)));
+    auto emitted_sequences = read_emit_seq("../ess_files/test_seq.ess");
+    auto is_test_passed = emitted_sequences.size() == 2 &&
+                          emitted_sequences[0] == HMM::Emit_seq_t{2, 2, 1, 0, 1, 3, 2, 0, 0} &&
+                          emitted_sequences[1] == HMM::Emit_seq_t{3, 2, 1, 0};
 
     return 1 - static_cast<int>(is_test_passed);
 }
