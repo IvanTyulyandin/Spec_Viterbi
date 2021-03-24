11153943f420f34ccf20b499ddd7360f916b2d9f
IvanTyulyandin <ivan.tyulyandin@gmail.com>
2021-03-24 22:56:01 +0300

    Small refactor of tests internals

--- tests/test_helper.h
+++ tests/test_helper.h
@@ -14,25 +14,25 @@ const auto expected_res = HMM::Mod_prob_vec_t{25.6574, 24.4874, HMM::to_modified
 
 constexpr auto LEVELS_TO_TEST = 7;
 
-bool compare_res_with_expected(const HMM::Mod_prob_vec_t& res) {
+bool compare_two_answers(const HMM::Mod_prob_vec_t& lhs, const HMM::Mod_prob_vec_t& rhs) {
     auto are_equal = true;
-    for (size_t i = 0; i < expected_res.size(); ++i) {
-        are_equal &= HMM::almost_equal(res[i], expected_res[i]);
+    for (size_t i = 0; i < lhs.size(); ++i) {
+        are_equal &= HMM::almost_equal(lhs[i], rhs[i]);
     }
-    are_equal &= (res.size() == expected_res.size());
+    are_equal &= (lhs.size() == rhs.size());
 
     return are_equal;
 }
 
 bool test_impl(const Viterbi_impl& impl) {
     auto res = impl.run_Viterbi(hmm, seq);
-    return compare_res_with_expected(res);
+    return compare_two_answers(res, expected_res);
 }
 
 bool test_spec_impl(Viterbi_spec_impl& impl) {
     impl.spec_with(hmm);
     auto res = impl.run_Viterbi_spec(seq);
-    return compare_res_with_expected(res);
+    return compare_two_answers(res, expected_res);
 }
 
 } // namespace test_helper
