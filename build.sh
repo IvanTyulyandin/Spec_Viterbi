a8f0a66392520d95b11ee0020dbfa2f2885bac43
IvanTyulyandin <ivan.tyulyandin@gmail.com>
2021-05-04 22:03:21 +0300

    Remove redundant option in build script

--- build.sh
+++ build.sh
@@ -8,11 +8,6 @@ CMAKE_ARGS='
     -DCMAKE_C_COMPILER=gcc \
     -DCMAKE_CXX_COMPILER=g++ \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \'
-case "$1" in
-    -C)
-        CMAKE_ARGS="$CMAKE_ARGS -DUSE_CUDA_CUSP=ON \\"
-        ;;
-esac
 
 cmake $CMAKE_ARGS ..
 make -j 5
